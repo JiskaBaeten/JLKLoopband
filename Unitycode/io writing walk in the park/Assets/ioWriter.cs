aa4ed2085b46473127fcb9d2e3c4378b5ca89125
LLeirs <laure.leirs@student.kdg.be>
2016-10-09 20:58:15 +0200

    io writer class added

--- Unitycode/io writing walk in the park/Assets/ioWriter.cs
+++ Unitycode/io writing walk in the park/Assets/ioWriter.cs
@@ -2,40 +2,40 @@
 using System.Collections;
 using System.IO;
 
-public class ioWriter : MonoBehaviour {
+public class ioWriter : MonoBehaviour
+{
 
-
-
+    profile testProfile;
+    string[] dataSplitted;
     StreamReader sReader;
     StreamWriter sWriter;
+    profile newProfile;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
+        readPatientData();
+        writePatientData();
+    }
 
-        try{ 
-        sWriter = File.CreateText("\\Assets\\userData\\test.txt");
-        sWriter.WriteLine("profielnummer");
-        sWriter.WriteLine("naam");
-        sWriter.WriteLine("niveau");
-        sWriter.WriteLine("snelheid");
-        }
-        catch
-        {
+    // Update is called once per frame
+    void Update()
+    {
 
-        }
-        finally {
-            sWriter.Close();
-        }
-
+    }
+    private void readPatientData()
+    {
         try
         {
-            sReader = File.OpenText("test.txt");
+            sReader = File.OpenText("profiles.txt");
             string userData = sReader.ReadLine();
             while (userData != null)
             {
-                Debug.Log(userData);
+                dataSplitted = userData.Split(';');
+                Debug.Log(dataSplitted[1]);
+                newProfile = new profile(dataSplitted[0], dataSplitted[1], dataSplitted[2]);
                 userData = sReader.ReadLine();
             }
-           
+
 
         }
         catch
@@ -47,9 +47,51 @@ public class ioWriter : MonoBehaviour {
             sReader.Close();
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private void writePatientData()
+    {
+        try
+        {
+            sWriter = File.CreateText("profilesTest1.txt");
+            sWriter.WriteLine(newProfile.writePatientData());
+        }
+        catch
+        {
+
+        }
+        finally
+        {
+            sWriter.Close();
+        }
+    }
+}
+public class profile
+{
+    string userNumber;
+    string name;
+    string skill;
+    public profile() { }
+
+    public profile(string tUserNumber, string tName, string tSkill)
+    {
+        userNumber = tUserNumber;
+        name = tName;
+        skill = tSkill;
+    }
+    public string UserNumber
+    {
+        get { return userNumber; }
+    }
+    public string UserName
+    {
+        get { return name; }
+    }
+    public string UserSkill
+    {
+        set { skill = value; }
+        get { return skill; }
+    }
+    public string writePatientData()
+    {
+        return name + ';' + userNumber + ';' + skill;
+    }
 }
