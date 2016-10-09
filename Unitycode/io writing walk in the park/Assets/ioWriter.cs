ffb0954a6514488f062178fcf7759003c3b2f9f1
LLeirs <laure.leirs@student.kdg.be>
2016-10-09 14:20:59 +0200

    adding io

---
+++ Unitycode/io writing walk in the park/Assets/ioWriter.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class ioWriter : MonoBehaviour {
+
+
+
+    StreamReader sReader;
+    StreamWriter sWriter;
+    // Use this for initialization
+    void Start () {
+
+        try{ 
+        sWriter = File.CreateText("\\Assets\\userData\\test.txt");
+        sWriter.WriteLine("profielnummer");
+        sWriter.WriteLine("naam");
+        sWriter.WriteLine("niveau");
+        sWriter.WriteLine("snelheid");
+        }
+        catch
+        {
+
+        }
+        finally {
+            sWriter.Close();
+        }
+
+        try
+        {
+            sReader = File.OpenText("test.txt");
+            string userData = sReader.ReadLine();
+            while (userData != null)
+            {
+                Debug.Log(userData);
+                userData = sReader.ReadLine();
+            }
+           
+
+        }
+        catch
+        {
+
+        }
+        finally
+        {
+            sReader.Close();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
