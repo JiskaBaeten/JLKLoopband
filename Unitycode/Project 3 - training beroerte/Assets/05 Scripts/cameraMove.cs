feefb0c20a126417355b71f93420d443ed677abf
LLeirs <laure.leirs@student.kdg.be>
2016-10-25 18:53:24 +0200

    demo ready

--- Unitycode/Project 3 - training beroerte/Assets/05 Scripts/cameraMove.cs
+++ Unitycode/Project 3 - training beroerte/Assets/05 Scripts/cameraMove.cs
@@ -11,7 +11,7 @@ public class cameraMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = Vector3.Lerp(transform.position, new Vector3( dog.transform.position.x + 1, transform.position.y, dog.transform.position.z + 1), 0.1f);
+       // transform.position = Vector3.Lerp(transform.position, new Vector3( dog.transform.position.x + 1, transform.position.y, dog.transform.position.z + 1), 0.1f);
             if (Input.GetKey(KeyCode.RightArrow))
             {
                 gameObject.transform.Rotate(0, 1, 0);
@@ -28,6 +28,23 @@ public class cameraMove : MonoBehaviour {
             {
                 gameObject.transform.Rotate(-1, 0, 0);
             }
+        if (Input.GetKey(KeyCode.Z))
+        {
+            gameObject.transform.position += new Vector3( 0,0, -0.5f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.position += new Vector3(0, 0, 0.5f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += new Vector3(-0.5f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            gameObject.transform.position += new Vector3(0.5f, 0, 0);
+        }
+
            
     }
 }
