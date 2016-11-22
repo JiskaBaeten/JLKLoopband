f8622fdb82f1a33c67cce94cfa48c079b7ed7c9c
LLeirs <laure.leirs@student.kdg.be>
2016-11-22 09:24:33 +0100

    import speech recognition + instructions

---
+++ Unitycode/Project 3 - training beroerte/Assets/05 Scripts/VoiceCommands.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Windows.Speech;
+using System.Collections.Generic;
+using System.Linq;
+
+
+public class VoiceCommands : MonoBehaviour {
+    KeywordRecognizer keywordRecognizer;
+    Dictionary<string, System.Action> keywords;
+    // Use this for initialization
+    void Start () {
+        Debug.Log("voice commands work");
+        keywords = new Dictionary<string, System.Action>();
+        keywords.Add("how are you", () => {Debug.Log("marvelous");});
+        keywords.Add("bark", () => {Debug.Log("woof");});
+        keywords.Add("activate", () => {Debug.Log("activated"); });
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        // if the keyword recognized is in our dictionary, call that Action.
+        if (keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
