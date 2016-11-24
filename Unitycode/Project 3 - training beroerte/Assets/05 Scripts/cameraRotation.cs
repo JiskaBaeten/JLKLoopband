f01b76c583def657eb6514ecc48953ab8287affa
LLeirs <laure.leirs@student.kdg.be>
2016-11-24 15:23:17 +0100

    try camera rotation

---
+++ Unitycode/Project 3 - training beroerte/Assets/05 Scripts/cameraRotation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraRotation : MonoBehaviour {
+    Vector3 currentCameraRotation;
+   public Vector3 rotationCameraLeft;
+   public Vector3 rotationCameraRight;
+	// Use this for initialization
+	void Start () {
+        currentCameraRotation = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x < currentCameraRotation.x)
+        {
+            transform.Rotate(rotationCameraLeft);
+        }
+        else if (transform.position.x > currentCameraRotation.x)
+        {
+            transform.Rotate(rotationCameraRight);
+        }
+        currentCameraRotation = transform.position;
+	}
+}
