af4dd52d31514e6591f3f0156cc105bac0017f38
Kirsten <kirsten.verhulst@student.kdg.be>
2016-12-05 14:17:00 +0100

    assets added to park scene for arduino

---
+++ Unitycode/Project 3 - training beroerte/Assets/05 Scripts/MessageListener.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MessageListener : MonoBehaviour {
+
+    // Invoked when a line of data is received from the serial device.
+    void OnMessageArrived(string msg)
+    {
+        
+    }
+
+    // Invoked when a connect/disconnect event occurs. The parameter 'success'
+    // will be 'true' upon connection, and 'false' upon disconnection or
+    // failure to connect.
+    void OnConnectionEvent(bool success)
+    {
+       
+    }
+}
