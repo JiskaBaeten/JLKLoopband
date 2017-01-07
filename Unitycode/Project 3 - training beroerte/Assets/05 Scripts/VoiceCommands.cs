b5d10014174cfe277e2a2006f7e838896c028cbf
LLeirs <laure.leirs@student.kdg.be>
2017-01-07 17:33:43 +0100

    dog home behaviour first add

--- Unitycode/Project 3 - training beroerte/Assets/05 Scripts/VoiceCommands.cs
+++ Unitycode/Project 3 - training beroerte/Assets/05 Scripts/VoiceCommands.cs
@@ -12,8 +12,10 @@ public class VoiceCommands : MonoBehaviour {
     steeringBehaviourDog dogSteeringBehaviourScript;
     GameObject viveCam;
     byte distanceToDogForLeash;
+    Renderer dogRenderer; 
     // Use this for initialization
     void Start () {
+       dogRenderer =  dog.GetComponent<Renderer>();
         distanceToDogForLeash = 5;
         dog = GameObject.FindWithTag("Dog");
         viveCam = GameObject.FindWithTag("cameraTopObject");
@@ -21,20 +23,63 @@ public class VoiceCommands : MonoBehaviour {
         animationControllerDog = dog.GetComponent<Animator>();
         keywords = new Dictionary<string, System.Action>();
         keywords.Add("bark", () => {Debug.Log("woof");});
-
+        
         keywords.Add("come", () => {
+            Debug.Log("coming");
+            if (animationControllerDog.GetBool("dogIsLoose"))
+            {
+                if (dogRenderer.isVisible)
+                {
+                    Debug.Log("hond in sight");
+                }
+                dogSteeringBehaviourScript.dogLookingForCall = true;
 
+                animationControllerDog.SetBool("dogIsWaiting", true);
+                dogSteeringBehaviourScript.resetTimerDogLooking();
+                if (UnityEngine.Random.Range(0, 3) >= 1)
+                {
+                    Debug.LogError("dog called and coming");
+                    dogSteeringBehaviourScript.dogCalledInScript = true;
 
+                }
+                else
+                {
+                    Debug.LogError("dog called and not coming");
+                    dogSteeringBehaviourScript.dogCalledInScript = false;
+                }
+            }
         });
 
         keywords.Add("kom", () => {
-            
+            if (animationControllerDog.GetBool("dogIsLoose"))
+            {
+                if (dogRenderer.isVisible)
+                {
+                    Debug.Log("hond in sight");
+                }
+                dogSteeringBehaviourScript.dogLookingForCall = true;
 
+                animationControllerDog.SetBool("dogIsWaiting", true);
+                dogSteeringBehaviourScript.resetTimerDogLooking();
+                if (UnityEngine.Random.Range(0, 3) >= 1)
+                {
+                    Debug.LogError("dog called and coming");
+                    dogSteeringBehaviourScript.dogCalledInScript = true;
+
+                }
+                else
+                {
+                    Debug.LogError("dog called and not coming");
+                    dogSteeringBehaviourScript.dogCalledInScript = false;
+                }
+            }
+            Debug.Log("ik kom");
 
         });
         keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
         keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
         keywordRecognizer.Start();
+        
     }
 
     private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
---
+++ Unitycode/Project 3 - training beroerte/Assets/homeSteeringBehaviourDog.cs
@@ -0,0 +1,175 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class homeSteeringBehaviourDog : MonoBehaviour {
+    //character control parameters
+    public int maxForce = 150;
+    public float mass = 100;
+    public float gravity = 9.81f;
+    public int maxRunningSpeed = 2;
+    float rotateSpeed = 0.5f;
+    float tmrDogFree;
+    float maxWanderTime = 3;
+    float wanderDist;
+    float wanderRadius;
+
+    //steer forces
+    public Vector3 velocity;
+    public Vector3 acceleration;
+    public Vector3 steerForce;
+    public Vector3 eindpos;
+
+
+
+    //obstacle avoidance
+    public float ObstacleAvoidanceDistance;
+    public cameraSteeringBehaviour cameraMoveScript;
+    GameObject cameraPlayer;
+    public float ObstacleAvoidanceForce;
+
+    CharacterController controller;
+    public Animator animationController;
+    // Use this for initialization
+    void Start () {
+        wanderDist = 2;
+        wanderRadius = 2;
+        controller = GetComponent<CharacterController>();
+        animationController = GetComponent<Animator>();
+        cameraPlayer = GameObject.FindWithTag("cameraTopObject");
+        eindpos = transform.position + transform.forward * wanderDist + Random.onUnitSphere * wanderRadius;
+        eindpos.y = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        //calc movement
+        steerForce = dogLooseBehaviour();
+        Truncate(ref steerForce, maxForce);// not > max
+        acceleration = steerForce / mass;
+        velocity += acceleration;//velocity = transform.TransformDirection(velocity);
+        Truncate(ref velocity, maxRunningSpeed);
+
+
+
+        if (controller.isGrounded)
+        {
+            controller.Move(velocity * Time.deltaTime);//move
+        }
+        else
+        {
+            controller.Move(new Vector3(0, -gravity * Time.deltaTime, 0));//fall down
+        }
+
+        //rotate
+        if (new Vector3(velocity.x, 0, velocity.z) != Vector3.zero)//otherwise warning
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(velocity.x, 0, velocity.z)), rotateSpeed * Time.deltaTime);
+        }
+    }
+    private void Truncate(ref Vector3 myVector, int myMax)//not above max
+    {
+        if (myVector.magnitude > myMax)
+        {
+            myVector.Normalize();// Vector3.normalized returns this vector with a magnitude of 1
+            myVector *= myMax;//scale to max
+        }
+    }
+
+    public Vector3 Seek(Vector3 seekPosition)
+    {
+        Vector3 mySteeringForce = (seekPosition - transform.position).normalized * maxForce;//look at target direction, normalized and scaled
+        Debug.DrawLine(transform.position, seekPosition, Color.cyan);
+        return mySteeringForce;
+    }
+
+    public Vector3 dogLooseBehaviour()
+    {
+
+        ObstacleAvoidance();
+        tmrDogFree += Time.deltaTime;
+        if (tmrDogFree > maxWanderTime || Vector3.Distance(eindpos, transform.position) < 1)
+        {
+            eindpos = Vector3.zero;
+            tmrDogFree = 0;
+
+            //heading = velocity.normalized;//where we're going                  //where we are + forward + random         
+            eindpos = transform.position + transform.forward * wanderDist + Random.onUnitSphere * wanderRadius;
+            eindpos.y = transform.position.y;//same height     
+        
+       
+
+           
+        }
+        return Seek(eindpos);
+    }
+
+    public Vector3 ObstacleAvoidance()
+    {
+
+        //two ray system, like antenna's
+        //one extra ray for the middle
+        Vector3 mySteeringForceL = Vector3.zero;
+        Vector3 mySteeringForceR = Vector3.zero;
+        Vector3 mySteeringForceM = Vector3.zero;
+        Vector3 directionL = 2 * transform.forward - transform.right;
+        Vector3 directionR = 2 * transform.forward + transform.right;
+        Vector3 directionM = transform.forward;
+        RaycastHit hitR;
+        RaycastHit hitL;
+        RaycastHit hitM;
+        //check for obstacle
+        bool hitObstacleOnTheLeft = DetectObstacle(directionL, out hitL,
+                                    ObstacleAvoidanceDistance);
+        bool hitObstacleOnTheRight = DetectObstacle(directionR, out hitR,
+                                     ObstacleAvoidanceDistance);
+        bool hitObstacleInTheMiddle = DetectObstacle(directionM, out hitM,
+                                      ObstacleAvoidanceDistance);
+        //obstacle found
+        if (hitObstacleOnTheLeft || hitObstacleOnTheRight || hitObstacleInTheMiddle)
+        {
+
+            //calc forces for each direction
+            if (hitObstacleOnTheLeft) mySteeringForceL = CalcAvoidanceForce(hitL);
+            if (hitObstacleOnTheRight) mySteeringForceR = CalcAvoidanceForce(hitR);
+            if (hitObstacleInTheMiddle) mySteeringForceM = CalcAvoidanceForce(hitM);
+
+            //sum them
+            if (mySteeringForceL != Vector3.zero &&
+                mySteeringForceR != Vector3.zero &&
+                mySteeringForceM == Vector3.zero)
+            {//possible narrow pathway
+                Debug.Log("narrow");
+                return Vector3.zero;//keep on going 
+            }
+            else
+            {//full force
+             //   Debug.Log("avoiding");
+                tmrDogFree = maxWanderTime;
+                return ObstacleAvoidanceForce * (mySteeringForceL + mySteeringForceR +
+                           mySteeringForceM);//just return the sum of all three
+            }
+        }
+        return Vector3.zero;//no steering force  because no obstacle was detected}
+    }
+
+    private bool DetectObstacle(Vector3 myDirection, out RaycastHit myHit, float myObstacleAvoidanceDistance)
+    {
+        if (Physics.Raycast(transform.position, myDirection, out myHit,
+            ObstacleAvoidanceDistance))//raycast, if hit
+        {//myHit is out since you need it elsewhere too
+            return true;
+        }
+        return false;
+    }
+
+    private Vector3 CalcAvoidanceForce(RaycastHit myHit)
+    {
+        Vector3 mySteeringForce = Vector3.zero;//a zero force
+                                               //eind - begin
+        mySteeringForce += (transform.position - myHit.point).normalized *
+                           ObstacleAvoidanceForce / myHit.distance;//calc force
+        return mySteeringForce;
+    }
+}
+
